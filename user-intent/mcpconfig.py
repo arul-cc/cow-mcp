70745574ab7529cc58325ba56ee2defae0a527cc
devanath anil <devanath.a@continube.com>
2025-06-18 17:09:44 +0530

    removed comments and unused packages

--- user-intent/mcpconfig.py
+++ user-intent/mcpconfig.py
@@ -1,23 +1,3 @@
-import traceback
 from mcp.server.fastmcp import FastMCP
-from mcp.server.auth.settings import AuthSettings
-from typing import List
-from typing import Tuple
-import os
-import json
-import base64
-import signal
-import sys
-from random import randint
-import asyncio
-
-from mcp.server.auth.provider import OAuthAuthorizationServerProvider,AccessTokenT,AccessToken
-from mcp.server.auth.middleware.auth_context import get_access_token
-
-from constants import constants
-from utils import utils
-from utils.debug import logger
-from utils.auth import CCowOAuthProvider
-
 
 mcp = FastMCP("ComplianceCow")
