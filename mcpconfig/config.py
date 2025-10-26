d0252082ac7359ea2d75698673c74d98b3ef48a3
Arul Pandiyan. G <arul.g@continube.com>
2025-10-26 08:34:05 +0530

    mcp moved to version 2.0

--- mcpconfig/config.py
+++ mcpconfig/config.py
@@ -1,3 +1,24 @@
-from mcp.server.fastmcp import FastMCP
+# from mcp.server.fastmcp import FastMCP
+import fastmcp
+import os
+from constants import constants
 
-mcp = FastMCP("ComplianceCow")
+if not constants.ENABLE_CCOW_API_TOOLS:
+
+    port = os.environ.get('CCOW_MCP_SERVER_PORT', "")
+    portInInt = 0
+
+    try:
+        portInInt = int(port)
+        print(f"Starting the server with streamable on port {portInInt}")
+    except ValueError:
+        print("Starting the server with stdio.")
+        # print(f"Environment variable 'CCOW_MCP_SERVER_PORT' is not a valid integer: {port}")
+
+    # Initialize and run the server
+
+    if portInInt > 1:
+        fastmcp.settings.host = "0.0.0.0"
+        fastmcp.settings.port = portInInt
+
+mcp = fastmcp.FastMCP("ComplianceCow")
