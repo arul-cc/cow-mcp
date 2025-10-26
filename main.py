0f4d699253b41e56a146a73725f3eab1c85c24eb
Arul Pandiyan. G <arul.g@continube.com>
2025-10-26 08:34:05 +0530

    mcp moved to version 2.0

--- main.py
+++ main.py
@@ -3,35 +3,35 @@ import signal
 import sys
 import traceback
 
-from utils.debug import logger
-from utils.auth import CCowOAuthProvider
+# from mcp.server.auth.settings import AuthSettings
+
 from constants.constants import host
-from mcp.server.auth.settings import AuthSettings
 from mcpconfig.config import mcp
 from tools.general import general
+from utils.auth import CCowOAuthProvider
+from utils.debug import logger
 
-
-mcp_tools_to_be_included = os.getenv("MCP_TOOLS_TO_BE_INCLUDED", "rules").lower().strip()
+mcp_tools_to_be_included = os.getenv("MCP_TOOLS_TO_BE_INCLUDED", "rules,insights,workflow").lower().strip()
 
 MCP_TOOLS = [t.strip() for t in mcp_tools_to_be_included.split(",") if t.strip()]
 
 if "insights" in MCP_TOOLS:
+    from prompts.insights import insights
+    from resources.graphdb import graphdb
     from tools.assessments.config import config
     from tools.assessments.run import run
-    from tools.graphdb import graphdb
-    from tools.dashboard import dashboard
     from tools.assets import assets
+    from tools.dashboard import dashboard
+    from tools.graphdb import graphdb
     from tools.help import help
-    from resources.graphdb import graphdb
-    from prompts.insights import insights
 
 if "rules" in MCP_TOOLS:
-    from tools.rules import rules
     from prompts.rule import rule
+    from tools.rules import rules
 
 if "workflow" in MCP_TOOLS:
-    from tools.workflow import workflow
     from prompts.workflow import workflow
+    from tools.workflow import workflow
 
 
 def signal_handler(sig, frame):
@@ -49,7 +49,7 @@ if __name__ == "__main__":
 
     try:
         portInInt = int(port)
-        print(f"Starting the server with SSE on port {portInInt}")
+        print(f"Starting the server with streamable on port {portInInt}")
     except ValueError:
         print("Starting the server with stdio.")
         # print(f"Environment variable 'CCOW_MCP_SERVER_PORT' is not a valid integer: {port}")
@@ -64,8 +64,6 @@ if __name__ == "__main__":
             print("\nExiting due to Ctrl+C")
             exit(0)
     else:
-        mcp.settings.host = "0.0.0.0"
-        mcp.settings.port = portInInt
         # mcp.settings.auth = AuthSettings(issuer_url=host)
         # mcp._auth_server_provider = CCowOAuthProvider()
-        mcp.run(transport='sse')
+        mcp.run(transport='streamable-http')
